4fc286bd68599ece88edcb525ab4d1087bd47791
Jakub <jakub.bchyla@gmail.com>
2022-09-30 15:58:53 +0200

    04

---
+++ 03-loops-and-arrays/growable-arrays.ts
@@ -0,0 +1,8 @@
+let sportsTwo: string[] = ["Golf", "Cricket", "Tenis", "Swimming"];
+
+sportsTwo.push("Baseball");
+sportsTwo.push("Futbol");
+
+for(let tempSport of sportsTwo){
+    console.log(tempSport);
+}
